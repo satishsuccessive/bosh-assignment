aa8452415e2263e1be17c25e5bc529ffa9b8a7b4
successive22 <successive22@asds-MacBook-Pro-2.local>
2021-02-17 23:39:11 +0530

    added styles

--- src/module/TabPannel/TabPanel.js
+++ src/module/TabPannel/TabPanel.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState, useCallback} from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
@@ -8,6 +8,13 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import axios from 'axios';
 import UserDetails from '../UserDetails/UserDetails';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -58,38 +65,38 @@ function SimpleTabs() {
   const url = 'https://reqres.in/api/users';
   useEffect(() => {
     getAllusers();
-  },[]);
+  }, []);
   const getAllusers = () => {
-            axios.get(url)
-            .then((response) => {
-                const allUsers = response.data.data;
-                console.log(response.data.data, '>>>response');
-                getUsers(allUsers);
-            })
-            .catch(error => console.log(`Error: ${error}`));
+    axios.get(url)
+      .then((response) => {
+        const allUsers = response.data.data;
+        console.log(response.data.data, '>>>response');
+        getUsers(allUsers);
+      })
+      .catch(error => console.log(`Error: ${error}`));
   }
 
   const collectUserDetails = (id) => {
     axios.get(`${url}/${id}`)
-    .then((response) => {
+      .then((response) => {
         const allDetails = response.data.data;
         console.log(response.data.data, '>>> details response');
         setUserData(allDetails);
-        console.log(userData,'userDatauserData');
-    })
-    .catch(error => console.log(`Error: ${error}`));
-}
+        console.log(userData, 'userDatauserData');
+      })
+      .catch(error => console.log(`Error: ${error}`));
+  }
 
   const handleChange = (event, newValue) => {
-      console.log(newValue, 'new v');
+    console.log(newValue, 'new v');
     setValue(newValue);
   };
 
   const viewUserClick = useCallback(async (e, id) => {
-      setValue(1);
-      collectUserDetails(id);
+    setValue(1);
+    collectUserDetails(id);
 
-  },[])
+  }, [])
 
   return (
     <div className={classes.root}>
@@ -100,25 +107,31 @@ function SimpleTabs() {
         </Tabs>
       </AppBar>
       <TabPanel value={value} index={0}>
-      <table>
-                <tr>
-                <th></th>
-                <th width="150px" align="left">First Name</th>
-                <th>Last Name</th>
-                <th>Email</th>
-                </tr>
-        { users.length>0 && users.map((user, index) => {
-            return(
-                    <tr onClick={(e) => viewUserClick(e, user.id)} >
-                    <td><img src={user.avatar} style={{width:'70px',borderRadius:'50%'}}/></td>
-                    <td>{user.first_name}</td>
-                    <td>{user.last_name}</td>
-                    <td>{user.email}</td>
-                    </tr>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+          <TableRow>
+            <TableCell></TableCell>
+            <TableCell component="th" width="150px" align="left">First Name</TableCell>
+            <TableCell component="th">Last Name</TableCell>
+            <TableCell component="th">Email</TableCell>
+          </TableRow>
+          </TableHead>
+          {users.length > 0 && users.map((user, index) => {
+            return (
+              <TableBody>
+              <TableRow onClick={(e) => viewUserClick(e, user.id)} >
+                <TableCell><img src={user.avatar} style={{ width: '70px', borderRadius: '50%' }} /></TableCell>
+                <TableCell>{user.first_name}</TableCell>
+                <TableCell>{user.last_name}</TableCell>
+                <TableCell>{user.email}</TableCell>
+              </TableRow>
+              </TableBody>
             )
-        })
-    }
-    </table>
+          })
+          }
+        </Table>
+        </TableContainer>
       </TabPanel>
       <TabPanel value={value} index={1}>
         <UserDetails data={userData} />
--- src/module/UserDetails/UserDetails.js
+++ src/module/UserDetails/UserDetails.js
@@ -1,14 +1,51 @@
 import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
+import { InputLabel } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        '& > *': {
+            margin: theme.spacing(1),
+        },
+    },
+    large: {
+        width: theme.spacing(15),
+        height: theme.spacing(15),
+    },
+}));
 const UserDetails = (props) => {
-    const {data} = props;
-    return(
-        <>
-        <img src={data.avatar} style={{width:'70px',borderRadius:'50%'}}/>
-        <lable>First Name:</lable> <input type="text" value={data.first_name} />
-        <lable>Last Name:</lable> <input type="text" value={data.last_name} />
-        <lable>Email:</lable> <input type="text" value={data.email} />
-        </>  
+    const { data } = props;
+    const classes = useStyles();
+
+    return (
+        <Box display="flex">
+            <Avatar src={data.avatar} className={classes.large}/>
+            <form fullWidth>
+                <Box p={2}>
+                    <InputLabel>First Name:</InputLabel>
+                    <TextField fullWidth id="outlined-basic" variant="outlined" value={data.first_name} />
+                </Box>
+                <Box p={2}>
+                    <InputLabel margin={2}>Last Name:</InputLabel>
+                    <TextField fullWidth id="outlined-basic" variant="outlined" value={data.last_name} />
+                </Box>
+                <Box p={2}>
+                    <InputLabel variant="standard">Email:</InputLabel>
+                    <TextField fullWidth id="outlined-basic" variant="outlined" type="text" value={data.email} />
+                </Box>
+                <Box p={2} display="flex" justifyContent="flex-end">
+                    <Button size="large" variant="contained" color="primary">
+                        Submit
+                     </Button>
+                </Box>
+            </form>
+        </Box>
     )
 }
 
