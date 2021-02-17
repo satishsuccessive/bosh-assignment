3597cdfae12b78d45087b80057adbeec55a06138
successive22 <successive22@asds-MacBook-Pro-2.local>
2021-02-17 14:49:06 +0530

    added drawer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import TabPanel from './module/TabPannel/TabPanel';
-
+import {Header} from './components';
 function App() {
   return (
     <div className="App">
+      <Header/>
       <TabPanel />
     </div>
   );
